c60909ebfcafa2022dc092bed9d3d08854dc7685
Rezapu Obed <obed.ubaidillah@gmail.com>
2024-01-14 14:46:43 +0700

    refactor: uses auth and cors middleware

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,33 +4,39 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"simplelinkshortener/internal/app/handler"
-	"strings"
+	"simplelinkshortener/internal/app/middlewares"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func Init() {
 	r := mux.NewRouter()
 
-	allowedOrigins := strings.Split(os.Getenv("ORIGIN_ALLOWED"), ",")
-	log.Printf("Allowed: %v\n", allowedOrigins)
+	// allowedOrigins := strings.Split(os.Getenv("ORIGIN_ALLOWED"), ",")
+	// log.Printf("Allowed: %v\n", allowedOrigins)
 
-	corsHandler := handlers.CORS(
-		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
-		handlers.AllowedMethods([]string{"GET", "POST"}),
-		handlers.AllowedOrigins(allowedOrigins),
-	)
+	// corsHandler := handlers.CORS(
+	// 	handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	// 	handlers.AllowedMethods([]string{"GET", "POST"}),
+	// 	handlers.AllowedOrigins(allowedOrigins),
+	// )
 
-	r.HandleFunc("/", handler.HomeHandler).Methods("GET")
-	r.HandleFunc("/auth/register", handler.RegisterHandler).Methods("POST")
-	r.HandleFunc("/auth/login", handler.LoginHandler).Methods("POST")
-	r.HandleFunc("/shorten", handler.ShortenHandler).Methods("POST")
-	r.HandleFunc("/{shortURL}", handler.RedirectHandler).Methods("GET")
+	// r.Use(corsHandler)
+	r.Use(middlewares.UseCors)
 
-	r.Use(corsHandler)
+	publicRouter := r.NewRoute().Subrouter()
+	publicRouter.HandleFunc("/", handler.HomeHandler).Methods("GET")
+	publicRouter.HandleFunc("/auth/register", handler.RegisterHandler).Methods("POST")
+	publicRouter.HandleFunc("/auth/login", handler.LoginHandler).Methods("POST")
+	publicRouter.HandleFunc("/{shortURL}", handler.RedirectHandler).Methods("GET")
+
+	secureRouter := r.NewRoute().Subrouter()
+	secureRouter.Use(middlewares.UseAuth)
+
+	lazyRouter := r.NewRoute().Subrouter()
+	lazyRouter.Use(middlewares.UseLazyAuth)
+	lazyRouter.HandleFunc("/shorten", handler.ShortenHandler).Methods("POST")
 
 	port := ":8080"
 	fmt.Printf("Server is listening on port %s...\n", port)

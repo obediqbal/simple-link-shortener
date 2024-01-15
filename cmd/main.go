221897a51a51a4c496cbd83a22ff43f1f3c10f41
Rezapu Obed <obed.ubaidillah@gmail.com>
2024-01-15 16:58:02 +0700

    refactor: always load .env

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"simplelinkshortener/internal/app"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if len(os.Args) > 2 && os.Args[2] == "dev" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file", err)
-		} else {
-			log.Println("Loaded .env file")
-		}
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Failed loading .env")
+	} else {
+		log.Println("Loaded .env file")
 	}
 	fmt.Printf("Starting app!!7!\n")
 	app.Init()
